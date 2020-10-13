46d5211836b6e76c970fc91af34b7f5a170af4a8
younj1601 <younj1601@wrdsb.ca>
2020-10-13 17:28:05 +0000

    Finished the second task for 2.05

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
 /**
- *
- * @author 
+ *Finds the factors for the interger in the parameter
+ * @author Jackson
  */
 public class Main {
+
+  public static void factors(int num){
+    for(int i = 1; i <= num; i++){
+      if(num % i == 0){
+        System.out.println(i);
+      }
+    }
+
+  }
+
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    
+    factors(30);
     
   }
 }
